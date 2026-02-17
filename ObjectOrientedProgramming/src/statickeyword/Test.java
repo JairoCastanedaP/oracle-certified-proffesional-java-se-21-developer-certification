0880c35d134eda890960cb0fe5902fedba3f2d05
Jairo <jairoalejandroc1@gmail.com>
2026-02-17 00:45:37 -0500

    NamingConventions and OOP classes files are included

---
+++ ObjectOrientedProgramming/src/statickeyword/Test.java
@@ -0,0 +1,11 @@
+package statickeyword;
+
+public class Test {
+    public static void main(String[] args) {
+
+        Car.showCurrentSpeed(Car.currentSpeed);
+        Car.speedUp(50);
+        Car.speedUp(140);
+        Car.stop();
+    }
+}
