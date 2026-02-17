ad138d9ba65f67af92cce348d820f72e87a49d2f
Jairo <jairoalejandroc1@gmail.com>
2026-02-16 22:49:04 -0500

    Access Modifier Example file is included

---
+++ AccesModifier/src/packageone/Dog.java
@@ -0,0 +1,13 @@
+package packageone;
+
+public class Dog {
+    public static void main(String[] args) {
+        Cat myCat= new Cat();
+
+        myCat.publicMethod();
+        myCat.protectedMethod();
+        myCat.defaultMethod();
+        //myCat.privatetMethod();/error
+
+    }
+}
